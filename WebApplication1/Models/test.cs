28086f487545c28e5896c288daf9b32145cd4576
Jimmy Parker <parkerj4@etsu.edu>
2020-05-29 15:57:39 -0400

    MVC upload

---
+++ WebApplication1/Models/test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class test
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public bool isAlive { get; set; }
+    }
+}
